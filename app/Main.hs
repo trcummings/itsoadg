5c72afd36b1b7b46b859df6a03d418ea98ea3979
Thomsen Cummings <thomsencummings@gmail.com>
2018-05-16 15:33:53 -0700

    this shit movin across the screen bruh!

--- app/Main.hs
+++ app/Main.hs
@@ -20,11 +20,11 @@ screenWidth, screenHeight :: CInt
 
 initialSize = V2 screenWidth screenHeight
 
-newtype Position = Position (V2 Double) deriving Show
+newtype Position = Position (V2 CInt) deriving Show
 instance Component Position where
   type Storage Position = Map Position
 
-newtype Velocity = Velocity (V2 Double) deriving Show
+newtype Velocity = Velocity (V2 CInt) deriving Show
 instance Component Velocity where
   type Storage Velocity = Map Velocity
 
@@ -32,16 +32,18 @@ data Player = Player deriving Show
 instance Component Player where
   type Storage Player = Unique Player
 
-makeWorld "World" [''Position, ''Velocity, ''Player]
+data Texture = Texture SDL.Texture (V2 CInt)
+instance Component Texture where
+  type Storage Texture = Map Texture
+
+makeWorld "World" [''Position, ''Velocity, ''Player, ''Texture]
 
 type System' a = System World a
 
-playerSpeed = 170
-playerPos = V2 0 (-120)
-xmin = -100
-xmax = 100
+playerSpeed = 1
+playerPos = V2 320 240
+spriteSize = V2 32 32
 
-data Texture = Texture SDL.Texture (V2 CInt)
 
 loadTexture :: SDL.Renderer -> FilePath -> IO Texture
 loadTexture r filePath = do
@@ -59,12 +61,18 @@ renderTexture r (Texture t size) xy clip =
   in SDL.copy r t clip (Just (SDL.Rectangle xy dstSize))
 
 
-initSystems :: System' ()
-initSystems = void $
-  newEntity (Player, Position playerPos, Velocity $ V2 0 0)
+initSystems :: Texture -> System' ()
+initSystems t = void $
+  newEntity (
+     Player
+   , Position playerPos
+   , Velocity $ V2 0 0
+   , t)
 
-bumpX dirF = cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x `dirF` playerSpeed) 0)
-bumpY dirF = cmap $ \(Player, Velocity (V2 _ y)) -> Velocity (V2 0 (y `dirF` playerSpeed))
+bumpX dirF = cmap $ \(Player, Velocity (V2 x _)) ->
+  Velocity (V2 (x `dirF` playerSpeed) 0)
+bumpY dirF = cmap $ \(Player, Velocity (V2 _ y)) ->
+  Velocity (V2 0 (y `dirF` playerSpeed))
 
 -- x axis
 handleArrowEvent SDL.KeycodeA SDL.Pressed  = bumpX (-)
@@ -72,10 +80,10 @@ handleArrowEvent SDL.KeycodeA SDL.Released = bumpX (+)
 handleArrowEvent SDL.KeycodeD SDL.Pressed  = bumpX (+)
 handleArrowEvent SDL.KeycodeD SDL.Released = bumpX (-)
 -- y axis
-handleArrowEvent SDL.KeycodeW SDL.Pressed  = bumpY (+)
-handleArrowEvent SDL.KeycodeW SDL.Released = bumpY (-)
-handleArrowEvent SDL.KeycodeS SDL.Pressed  = bumpY (-)
-handleArrowEvent SDL.KeycodeS SDL.Released = bumpY (+)
+handleArrowEvent SDL.KeycodeW SDL.Pressed  = bumpY (-)
+handleArrowEvent SDL.KeycodeW SDL.Released = bumpY (+)
+handleArrowEvent SDL.KeycodeS SDL.Pressed  = bumpY (+)
+handleArrowEvent SDL.KeycodeS SDL.Released = bumpY (-)
 handleArrowEvent _ _ = return ()
 
 handleEvent :: SDL.Event -> System' ()
@@ -92,11 +100,15 @@ step :: [SDL.Event] -> SDL.Renderer -> System' ()
 step events renderer = do
   mapM handleEvent events
   cmap $ \(Position p, Velocity v) -> Position (p + v)
-  cmapM_ $ \(Player, Position p, Velocity v) -> do
+  cmapM_ $ \(Player, Position p, Velocity v, Texture t s) -> do
     liftIO $ SDL.rendererDrawColor renderer $= V4 0 0 0 0
     liftIO $ SDL.clear renderer
+    liftIO $ renderTexture
+      renderer
+      (Texture t s)
+      (P p)
+      (Just $ SDL.Rectangle (P (V2 0 0)) spriteSize)
     liftIO $ SDL.present renderer
-    liftIO $ putStrLn $ show (p, v)
   runGC
 
 appLoop :: SDL.Renderer -> World -> IO ()
@@ -110,7 +122,8 @@ main = do
   SDL.initializeAll
   window <- SDL.createWindow "In the Shadow of a Dead God" SDL.defaultWindow { SDL.windowInitialSize = initialSize }
   renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+  spriteSheetTexture <- loadTexture renderer "assets/red_square.bmp"
 
   world <- initWorld
-  runSystem initSystems world
+  runSystem (initSystems spriteSheetTexture) world
   appLoop renderer world
