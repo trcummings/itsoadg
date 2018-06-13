d9cafe29e656e477bf24804f5ac0eeffba3cd3bc
Thomsen Cummings <thomsencummings@gmail.com>
2018-06-13 10:25:20 -0700

    implement effect map fn with Apecs core to allow for queue event returning cmap calls

--- src/Game/Wrapper/Apecs.hs
+++ src/Game/Wrapper/Apecs.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Game.Wrapper.Apecs where
 
-import qualified Apecs as ECS
+import qualified Apecs               as ECS
+import qualified Apecs.Core          as Core
+import qualified Data.Vector.Unboxed as U
 import           Control.Monad.IO.Class (MonadIO)
 import           System.Mem             (performMajorGC)
+
+import           Game.World (World)
+import           Game.Types (QueueEvent)
 
 class Monad m => Apecs m where
   runSystem :: ECS.System w a -> w -> m a
@@ -15,3 +22,17 @@ runSystem' f w = ECS.liftIO $ ECS.runSystem f w
 
 runGC' :: MonadIO m => m ()
 runGC' = ECS.liftIO $ performMajorGC
+
+emap :: forall cx cy. (ECS.Has World cx, ECS.Has World cy)
+     => (cx -> (cy, [QueueEvent]))
+     -> ECS.System World [QueueEvent]
+emap f = do
+  sx :: ECS.Storage cx <- ECS.getStore
+  sy :: ECS.Storage cy <- ECS.getStore
+  sl <- ECS.liftIO $ Core.explMembers sx
+  qs <- ECS.liftIO $ U.foldM (\qs e -> do
+                                 x <- Core.explGet sx e
+                                 let (x', qs') = f x
+                                 Core.explSet sy e x'
+                                 return $ qs ++ qs' ) [] sl
+  return qs
