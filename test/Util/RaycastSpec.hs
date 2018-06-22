f27f7dd140fa0af0ca13675e309ca7d20d4b5962
Thomsen Cummings <thomsencummings@gmail.com>
2018-06-21 17:19:37 -0700

    beef up raycast test

--- test/Util/RaycastSpec.hs
+++ test/Util/RaycastSpec.hs
@@ -18,16 +18,22 @@ spec :: Spec
 spec = do
   describe "Game.Util.Raycast" $ do
     context "rayIntersection2d" $ do
+      let tinyRay  = Ray  { origin = V2 0.5 (1 + onePixel)  , delta = V2 0 0.5 }
+          shortRay = Ray  { origin = V2 0.5 (1 + onePixel)  , delta = V2 0 1 }
+          longRay  = Ray  { origin = V2 0.5 (1 + onePixel)  , delta = V2 0 5 }
+          box      = AABB { center = V2 0.5 2.5, dims = V2 1 1 }
       it "returns a hit when the ray just touches it" $ do
-        let ray = Ray  { origin = V2 0.5 (1 + onePixel)  , delta = V2 0 1 }
-            box = AABB { center = V2 0.5 2.5, dims  = V2 1 1 }
-            hit = rayIntersection2d ray box
+        let hit = rayIntersection2d shortRay box
         (distance <$> hit) `shouldBe` (Just $ V2 0 (1 - onePixel))
+        (normal <$> hit)   `shouldBe` (Just $ V2 0 1)
       it "returns the closest hit when the ray is longer than the box" $ do
-        let ray = Ray  { origin = V2 0.5 (1 + onePixel)  , delta = V2 0 5 }
-            box = AABB { center = V2 0.5 2.5, dims  = V2 1 1 }
-            hit = rayIntersection2d ray box
+        let hit = rayIntersection2d longRay box
         (distance <$> hit) `shouldBe` (Just $ V2 0 (1 - onePixel))
+        (normal <$> hit)   `shouldBe` (Just $ V2 0 1)
+      it "returns nothing when the ray is too short to hit the box" $ do
+        let hit = rayIntersection2d tinyRay box
+        (distance <$> hit) `shouldBe` Nothing
+        (normal <$> hit)   `shouldBe` Nothing
 
 main :: IO ()
 main = hspec spec
