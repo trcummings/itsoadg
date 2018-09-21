73cd22c99a0d401653bf8aa4308a77c3eba0b3d7
Thomsen Cummings <thomsencummings@gmail.com>
2018-09-21 15:12:13 -0700

    door cleanup

--- src/Game/System/Scratch/Door.hs
+++ src/Game/System/Scratch/Door.hs
@@ -1,11 +1,9 @@
 module Game.System.Scratch.Door where
 
-import           Game.Util.Move (defaultMoveable)
-import           Game.World.TH  (ECS, World)
-import           Game.World.Hierarchy
-  ( HierarchyCons(..)
-  , newHierarchicalEntity )
-import           Game.Types
+import Game.Util.Move       (defaultMoveable)
+import Game.World.TH        (ECS, World)
+import Game.World.Hierarchy (HierarchyCons(..), newHierarchicalEntity)
+import Game.Types
   ( Position3D(..)
   , Orientation(..)
   , Hierarchy(..)
@@ -18,12 +16,11 @@ initDoor = do
   let topPanel = (DoorPanel'Top,    defaultMoveable)
       botPanel = (DoorPanel'Bottom, defaultMoveable)
       door     = (Door, defaultMoveable)
-      door2    = (Door, defaultMoveable)
 
   newHierarchicalEntity
     Nothing
     (HierarchyCons
       (Door, defaultMoveable)
-      [ HierarchyCons topPanel [ HierarchyCons door2 [] ]
+      [ HierarchyCons topPanel []
       , HierarchyCons botPanel [] ] )
   return ()
